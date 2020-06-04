b21401578a813a31a6ac2770de4d17391b1c4c08
huangkun061147 <huangkun8070@163.com>
2020-06-04 14:41:15 +0800

    测试提交

--- src/main/java/com/opple/controller/UserController.java
+++ src/main/java/com/opple/controller/UserController.java
@@ -35,6 +35,6 @@ public class UserController {
     }
     @RequestMapping(value = "/test2", method = RequestMethod.GET)
     public String test2(){
-        return "2222";
+        return "22223";
     }
 }
